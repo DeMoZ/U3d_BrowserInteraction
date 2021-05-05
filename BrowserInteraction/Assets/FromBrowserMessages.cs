fff1df219188fa74ad14bafcfc1ae30d901d491b
Roman Kolychev <demozbox@gmail.com>
2021-05-05 17:15:25 +0300

    Browser interaction implemented.

---
+++ BrowserInteraction/Assets/FromBrowserMessages.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FromBrowserMessages : MonoBehaviour
+{
+    private static FromBrowserMessages instance = null;
+
+    [SerializeField] private Text text;
+
+    private void Start()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance == this) 
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SendString(string value)
+    {
+        text.text = value;
+        Debug.LogWarning($"SendString {value}");
+    }
+
+    public void LoadSceneByNumber(string value)
+    {
+        text.text = value;
+        Debug.LogWarning($"LoadSceneByNumber {value}");
+
+        if (int.TryParse(value, out var parsed))
+            SceneManager.LoadScene(parsed);
+    }
+
+    public void LoadSceneByName(string value)
+    {
+        text.text = value;
+        Debug.LogWarning($"LoadSceneByName {value}");
+
+        SceneManager.LoadScene(value);
+    }
+}
---
+++ BrowserInteraction/Assets/Plugins/CommunicateToBrowser.jslib
@@ -0,0 +1,8 @@
+mergeInto(LibraryManager.library, {
+    OnModelLoaded: function (str) {
+        var message = Pointer_stringify(str);
+        console.log("javascript: Unity loaded and can receive messages from browser. " + message);
+        // Pass message to the page
+        OnUnityModelLoaded(message); // This function is embeded into the page
+    },   
+});
