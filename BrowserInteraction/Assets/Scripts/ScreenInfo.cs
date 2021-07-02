046c8926ecd578246057146b56c2c6192a2498ae
Roman Kolychev <r_kolychev@controlsystems.ru>
2021-07-03 00:39:52 +0300

    Screen size in inches

---
+++ BrowserInteraction/Assets/Scripts/ScreenInfo.cs
@@ -0,0 +1,97 @@
+﻿using System.Text;
+using UnityEngine;
+
+public enum OsTypes
+{
+    Other,
+    Windows,
+    Mac,
+    Android,
+    Ios
+}
+
+public enum SystemTypes
+{
+    Desktop,
+    Tablet,
+    Phone
+}
+
+public static class ScreenInfo
+{
+    public static string ToString(float inch, string os, string systemType)
+    {
+        var strBuild = new StringBuilder();
+        strBuild.AppendLine(os);
+        strBuild.AppendLine(inch.ToString());
+        strBuild.AppendLine(Screen.dpi.ToString());
+        strBuild.AppendLine(Screen.width + "x" + Screen.height);
+        strBuild.AppendLine(systemType);
+            
+        strBuild.AppendLine(SystemInfo.operatingSystem);
+        strBuild.AppendLine("UNITY_WEBGL");
+        return strBuild.ToString();
+    }
+
+    public static float ViewSize() =>
+        ViewSize(out var os);
+
+    public static float ViewSize(out OsTypes os)
+    {
+        float inch = Screen.width * Screen.width + Screen.height * Screen.height;
+        inch = Mathf.Sqrt(inch);
+        os = GetOs();
+
+        if (os == OsTypes.Android)
+            inch /= (2 * Screen.dpi);
+        if (os == OsTypes.Ios)
+            inch /= (3 * Screen.dpi);
+        else
+            inch /= Screen.dpi;
+
+        return inch;
+    }
+
+    public static OsTypes GetOs()
+    {
+        var result = OsTypes.Other;
+        var systemName = SystemInfo.operatingSystem.Split(' ')[0];
+        // Windows ...  / Mac ... / iOS ... / Android ... / Other
+        switch (systemName)
+        {
+            case "Windows":
+                result = OsTypes.Windows;
+                break;
+            case "Mac":
+                result = OsTypes.Mac;
+                break;
+            case "iOS":
+                result = OsTypes.Ios;
+                break;
+            case "Android":
+                result = OsTypes.Android;
+                break;
+        }
+
+        return result;
+    }
+
+    public static SystemTypes GetSystem(float inch, OsTypes os)
+    {
+        var result = SystemTypes.Desktop;
+        switch (os)
+        {
+            case OsTypes.Mac:
+            case OsTypes.Windows:
+            case OsTypes.Other:
+                result = (inch < 13) ? SystemTypes.Tablet : SystemTypes.Desktop;
+                break;
+            case OsTypes.Ios:
+            case OsTypes.Android:
+                result = (inch < 8) ? SystemTypes.Phone : SystemTypes.Tablet;
+                break;
+        }
+
+        return result;
+    }
+}
---
+++ BrowserInteraction/Assets/Scripts/ScreenJobs.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenJobs : MonoBehaviour
+{
+    [SerializeField] private CanvasScaler canvasScaler = default;
+    [SerializeField] private Vector2 desktopResolution = default;
+    [SerializeField] private Vector2 tabletResolution = default;
+    [SerializeField] private Vector2 phoneResolution = default;
+
+    [SerializeField] private bool showOnScreen = false;
+    [SerializeField] private Text infoField = default;
+
+    void Start()
+    {
+#if UNITY_WEBGL
+        var inch = ScreenInfo.ViewSize(out var os);
+        var systemType = ScreenInfo.GetSystem(inch, os);
+
+        switch (systemType)
+        {
+            case SystemTypes.Desktop:
+                SetUiResolution(desktopResolution);
+                break;
+            case SystemTypes.Tablet:
+                SetUiResolution(tabletResolution);
+                break;
+            default:
+                SetUiResolution(phoneResolution);
+                break;
+        }
+
+        var log = ScreenInfo.ToString(inch, os.ToString(), systemType.ToString());
+        Debug.Log(log);
+
+        if (showOnScreen)
+            infoField.text = log;
+
+        canvasScaler.gameObject.SetActive(enabled);
+#endif
+    }
+
+    private void SetUiResolution(Vector2 resolution) =>
+        canvasScaler.referenceResolution = resolution;
+}
